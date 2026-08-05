d1c0e428f79bb0d059ab3882c4b7f86be2838a3a
zeki akgül <121755454+zexy2@users.noreply.github.com>
2026-08-06 00:54:00 +0300

    build: publish root-base bundle for custom domain

--- docs/registerSW.js
+++ docs/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Postify-blog/sw.js', { scope: '/Postify-blog/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
